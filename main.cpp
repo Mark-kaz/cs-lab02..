f6fb8a8867a48e71544932c0611e564c589f26fd
Alice (AntimonovM) <alice@example.com>
2020-03-22 01:59:06 +0300

    code: нахождение максимума

--- main.cpp
+++ main.cpp
@@ -13,6 +13,13 @@ int main()
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
-
+    if (a>b)
+    {
+        cout <<"max="<<a;
+    }
+    else
+    {
+        cout <<"max="<<b;
+    }
     return 0;
 }
